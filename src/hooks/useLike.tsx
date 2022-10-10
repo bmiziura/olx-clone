bab53d12fd2c8b89d53d789e356c0101a5f2fb93
goblicpl <bartosz.miziura@o2.pl>
2022-10-10 15:05:18 +0200

    Changed login url inside useLike hook

--- src/hooks/useLike.tsx
+++ src/hooks/useLike.tsx
@@ -25,7 +25,7 @@ function useLike(id: string) {
 
   const likePostHandler = async () => {
     if (!data?.user) {
-      router.push("/api/auth")
+      router.push("/api/auth/signin")
       return
     }
 
